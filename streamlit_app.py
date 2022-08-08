5614d7c572481a15e26c5a6e06363ac587799c4a
Prometheus4587 <69340526+Prometheus4587@users.noreply.github.com>
2022-08-08 12:45:01 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Moms New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('\N{U+1F963}', 'Omrga 3 & blueberry Oatmeal')
+streamlit.text(':smiley:', 'Omrga 3 & blueberry Oatmeal')
 streamlit.text('\N{U+1F959}', ' Kale, Spinach & Rocket Smoothie')
 streamlit.text('\N{U+1F414}', ' Hard-Boiled Free Range Egg')
 streamlit.text('\N{U+1F951}', '\N{U+1F35E}', ' Avacado Toast')
