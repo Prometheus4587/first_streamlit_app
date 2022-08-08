efae2fc7e3f412df778208e864abec98173f4f73
Prometheus4587 <69340526+Prometheus4587@users.noreply.github.com>
2022-08-08 12:48:19 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Moms New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.markdown('\N{:smiley:} Omega 3 & blueberry Oatmeal')
+streamlit.text('\N{:smiley:} Omega 3 & blueberry Oatmeal')
 
 
