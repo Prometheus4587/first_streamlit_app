e827139c2f04a7a47a39c14f452797574681285d
Prometheus4587 <69340526+Prometheus4587@users.noreply.github.com>
2022-08-08 12:21:31 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 streamlit.title('My Moms New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('/N{U+1F963} Omrga 3 & blueberry Oatmeal')
-streamlit.text('/N{U+1F959} Kale, Spinach & Rocket Smoothie')
-streamlit.text('/N{U+1F414} Hard-Boiled Free Range Egg')
-streamlit.text('/N{U+1F951}/N{U+1F35E} Avacado Toast')
-Streamlit.header('/N{U+1F34C}/N{U+1F96D} Build Your Own Fruit Smoothie /N{U+1F95D}/N{U+1F347}')
+streamlit.text('\N{U+1F963} Omrga 3 & blueberry Oatmeal')
+streamlit.text('\N{U+1F959} Kale, Spinach & Rocket Smoothie')
+streamlit.text('\N{U+1F414} Hard-Boiled Free Range Egg')
+streamlit.text('\N{U+1F951}\N{U+1F35E} Avacado Toast')
+streamlit.header('\N{U+1F34C}\N{U+1F96D} Build Your Own Fruit Smoothie \N{U+1F95D}\N{U+1F347}')
 
