8a76e558a2414c6a9b48e74598ca75c9f867cf36
Prometheus4587 <69340526+Prometheus4587@users.noreply.github.com>
2022-08-08 11:58:45 +0600

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents first dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omrga 3 & blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free Range Egg')
